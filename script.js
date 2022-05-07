5d86405636788a723d8926dd9ff232c9e43b9174
Dmitry <dmitrii.z@fireart-d.com>
2022-05-07 12:40:02 +0200

    ver-2

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 class Me {
   constructor(name, surname, position, email, phone, city, experience, skills, tech, interests) {
-    this.name = name
-    this.surname = surname
-    this.position = position
-    this.email = email
-    this.phone = phone
-    this.experience = experience
-    this.skills = skills
-    this.city = city
-    this.tech = tech
-    this.interests = interests
+    this.name = name;
+    this.surname = surname;
+    this.position = position;
+    this.email = email;
+    this.phone = phone;
+    this.experience = experience;
+    this.skills = skills;
+    this.city = city;
+    this.tech = tech;
+    this.interests = interests;
   }
 }
 
 class City {
-    name
-    country
-    constructor(name, country) {
-        this.name = name;
-        this.country = country
-      }
+  name;
+  country;
+  constructor(name, country) {
+    this.name = name;
+    this.country = country;
+  }
 }
 
 class Company {
-  title
-  city
+  title;
+  city;
 
   constructor(title, city) {
     this.title = title;
-    this.city = city
+    this.city = city;
   }
 }
 class Project {
-    constructor(title, link) {
-        this.title = title
-        this.link = link
-    }
+  constructor(title, link) {
+    this.title = title;
+    this.link = link;
+  }
 }
 
 class Experience {
@@ -43,7 +43,7 @@ class Experience {
   end;
   company;
   description;
-  position
+  position;
 
   constructor(start, end, company, position, description) {
     this.start = start;
@@ -55,20 +55,20 @@ class Experience {
 }
 
 //variables
-let me
-let back = document.getElementById('back')
+let me;
+let back = document.getElementById('back');
 const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
 //
 function createCV() {
-  back.style.visibility ='hidden'
-  me = new Me("Dmitry", "Zimoykin", 'Sowtware developer',"zimoykin@gmail.com", '+7 96O7O1777O',new City ('Moscow', 'Russia'),
-    getExperience().sort( (a,b) => { a.start > b.start }),
+  back.style.visibility = 'hidden';
+  me = new Me("Dmitrii", "Zimoikin", 'Backend developer', "zimoykin@gmail.com", '+48 737 404 142', new City('Warsaw', 'Poland'),
+    getExperience().sort((a, b) => { a.start > b.start; }),
     getMySkills(), getMyTech(), getMyInterests()
-)
+  );
 
   console.log(me);
   setTimeout(() => {
-    fillHtmlTemplate(me)
+    fillHtmlTemplate(me);
   }, 1000);
 
 }
@@ -76,17 +76,23 @@ function createCV() {
 
 function getExperience() {
 
-  const moscow = new City('Moscow', 'Russia')
-  const fichtenberg = new City ('Fichtenberg', 'Germany')
+  const moscow = new City('Moscow', 'Russia');
 
   return [
-    // new Experience(
-    //   new Date(2020, 10, 01),
-    //   null,
-    //   new Company('RaceRoom Entertainment GmbH', 'Fichtenberg', 'Germany'),
-    //   'Software Developer',
-    //   "Participating in the development of microservices based swift and javascript."
-    // ),
+    new Experience(
+      new Date(2021, 08, 01),
+      null,
+      new Company('Fireart Studio', new City('Warsaw', 'Poland')),
+      'Backend Developer',
+      "Nodejs developer "
+    ),
+    new Experience(
+      new Date(2020, 10, 01),
+      null,
+      new Company('RaceRoom Entertainment GmbH', new City('Fichtenberg', 'Germany')),
+      'Software Developer',
+      "Participating in the development of microservices based swift and javascript. Android application (kotlin, jetpack compose)"
+    ),
     new Experience(
       new Date(2017, 12, 01),
       null,
@@ -127,67 +133,85 @@ function getExperience() {
   ];
 }
 function getMySkills() {
-    return [
-        'Swift', 'TypeScript(JavaScript)', 'JAVA', 'SQL'
-    ]
+  return [
+    'TypeScript(JavaScript)', 'Kotlin', 'Swift', 'SQL'
+  ];
 }
 function getMyTech() {
-    return [
-        'Vapor', 'NodeJS (ExpressJS NestJS)', 'Angular', 'Docker', 'ReactNative', 'Rails', 'Git', 'RESTApi', 'mongoDB', 'Redis', 'Postgresql'
-    ]
+  return [
+    'NodeJS (ExpressJS/NestJS)', 'Vapor', 'Docker', 'Git', 'RESTApi', 'Postgresql', 'Redis', 'DynamoDB'
+  ];
 }
 function getMyInterests() {
-    return [
-        'backend', 'web', 'mobile', 'UX', 'database'
-    ]
+  return [
+    'backend', 'web', 'mobile', 'UX', 'database'
+  ];
 }
 //html interactiove
 function fillHtmlTemplate(me) {
 
-    document.getElementById('me').innerHTML = `${me.name} ${me.surname}`
+  document.getElementById('me').innerHTML = `${me.name} ${me.surname}`;
 
-    let div = document.getElementById('left')
-    _ = me.experience.map ( val => {
-        let company=document.createElement("h3")
-        company.innerHTML = val.company.title + ' (' + val.company.city.name + ', ' + val.company.city.country + ')'
-        div.appendChild(company)
-        let position=document.createElement("span")
-        position.innerHTML = val.position + ', since ' + months[val.start.getMonth()] + ' ' + val.start.getFullYear() + ' - ' + (val.end === null ? 'Present' :  months[val.end.getMonth()] + ' ' + val.end.getFullYear())
-        div.appendChild(position)
-        let about=document.createElement("span")
-        _ = val.description.split("●").map( val => {
-          if (val) about.innerHTML += `<br>●${val}`
-        })
-        div.appendChild(about)
-        let stroke=document.createElement("hr")
-        div.appendChild(stroke)
-    })
+  let div = document.getElementById('exp');
+  _ = me.experience.map(val => {
+    let stroke = document.createElement("hr");
+    let company = document.createElement("h3");
+    company.innerHTML = val.company.title + ' (' + val.company.city.name + ', ' + val.company.city.country + ')';
+    div.appendChild(company);
+    let position = document.createElement("span");
+    position.innerHTML = val.position + ', since ' + months[val.start.getMonth()] + ' ' + val.start.getFullYear() + ' - ' + (val.end === null ? 'Present' : months[val.end.getMonth()] + ' ' + val.end.getFullYear());
+    div.appendChild(position);
+    // let about = document.createElement("span");
+    // _ = val.description.split("●").map(val => {
+    //   if (val) about.innerHTML += `<br>●${val}`;
+    // });
+    // div.appendChild(about);
+    div.appendChild(stroke);
+  });
 
-    let div_position = div.getBoundingClientRect()
-    div.style.height = Math.min(div_position.height, window.innerHeight - div_position.y ) - 50
+  const position = document.getElementById('position');
+  const positionName = document.createElement("h4");
+  positionName.innerHTML = me.position;
+  position.appendChild(positionName);
 
-}
-function toggleLocale() {
-    let locale = document.getElementById('lang')
-    if ( locale.innerHTML === '🇬🇧') { 
-      locale.innerHTML = '🇷🇺'
-    } else { 
-      locale.innerHTML = '🇬🇧'
-    }
+  const skills = document.getElementById('skills');
+  const skillName = document.createElement("h3");
+
+  skillName.innerHTML = 'Skills:';
+  skills.appendChild(skillName);
+
+  const skill = document.createElement("span");
+  skill.innerHTML = me.skills.join(', ');
+  skills.appendChild(skill);
+
+  //
+  const interests = document.getElementById('interests');
+  const interestName = document.createElement("h3");
+
+  interestName.innerHTML = 'Interest:';
+  interests.appendChild(interestName);
+
+  const interestValue = document.createElement("span");
+  interestValue.innerHTML = me.interests.join(', ');
+  interests.appendChild(interestValue);
+
+  //let div_position = div.getBoundingClientRect();
+  //div.style.height = Math.min(div_position.height, window.innerHeight - div_position.y) - 50;
+
 }
 function showPhone() {
-  back.style.visibility = (back.style.visibility === 'hidden') ? 'visible' : 'hidden'
-  back.style.opacity = 100
+  back.style.visibility = (back.style.visibility === 'hidden') ? 'visible' : 'hidden';
+  back.style.opacity = 100;
   if (me) {
-    document.getElementById('phone').innerHTML = me.phone
+    document.getElementById('phone').innerHTML = me.phone;
   }
 }
-function closeWindow(){
-  back.style.opacity = 0
-  back.style.visibility = (back.style.visibility === 'hidden') ? 'visible' : 'hidden'
+function closeWindow() {
+  back.style.opacity = 0;
+  back.style.visibility = (back.style.visibility === 'hidden') ? 'visible' : 'hidden';
 }
-function changeBackground() {
-  document.getElementById('up').style.backgroundColor = `rgb(${random(100,255)}, ${random(100,255)}, ${random(100,255)})`
+function changeBackground(value) {
+  document.getElementById(value.id).style.backgroundColor = `rgb(${random(100, 255)}, ${random(100, 255)}, ${random(100, 255)})`;
 
   function random(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
